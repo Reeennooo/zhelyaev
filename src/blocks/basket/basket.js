2086eecf4975ccab5551c446af3a63001cc9885c
Reeennooo <105305554+Reeennooo@users.noreply.github.com>
2023-12-15 16:31:12 +0300

    Правки путей

--- src/blocks/basket/basket.js
+++ src/blocks/basket/basket.js
@@ -27,7 +27,7 @@ class Product {
                 <span class='value'>${totalCost}</span>&nbsp;₽
             </div>
             <button class='basket-element__delete'>
-                <svg><use href='img/sprite.svg#bx-trash'></use></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"/><path d="M9 10h2v8H9zm4 0h2v8h-2z"/></svg>
             </button>
         </div>
         `;
